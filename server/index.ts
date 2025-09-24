a542b5f0e5d30cceff36ca80eeee6495bf952ad3
Anna Geller <anna.m.geller@gmail.com>
2025-09-24 10:24:42 +0200

    fix: replay deploy preview

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,12 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen(port, 'localhost', () => {
+  // server.listen(port, 'localhost', () => {
+  server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+  }, () => {
     log(`serving on port ${port}`);
   });
 })();
